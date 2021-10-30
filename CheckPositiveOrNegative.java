1f1d7941e624217d026865ec36b1b347b753cfed
PriyaMuthu-24 <93384681+PriyaMuthu-24@users.noreply.github.com>
2021-10-30 22:16:48 +0530

    Add files via upload

---
+++ CheckPositiveOrNegative.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class CheckPositiveOrNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number = -4;
+
+		if (number > 0) {
+			System.out.println("Number is positive");
+
+		}
+
+		else if (number < 0) {
+			System.out.println("Number is Negative ");
+		}
+
+		else {
+			System.out.println("The number is neither positive nor negative");
+
+		}
+
+	}
+}
